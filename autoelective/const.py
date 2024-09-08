7fa397a53f51f9331732e0961a0d455d0db38bb7
root <root@MagicBook14>
2024-09-08 11:06:12 +0800

    Initial commit

---
+++ autoelective/const.py
@@ -0,0 +1,73 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# filename: const.py
+# modified: 2019-09-11
+
+import os
+from ._internal import mkdir, get_abs_path, read_list
+
+CACHE_DIR = get_abs_path("../cache/")
+CAPTCHA_CACHE_DIR = get_abs_path("../cache/captcha/")
+LOG_DIR = get_abs_path("../log/")
+ERROR_LOG_DIR = get_abs_path("../log/error")
+REQUEST_LOG_DIR = get_abs_path("../log/request/")
+WEB_LOG_DIR = get_abs_path("../log/web/")
+
+CNN_MODEL_FILE = get_abs_path("../model/cnn.20210311.1.pt")
+USER_AGENTS_TXT_GZ = get_abs_path("../user_agents.txt.gz")
+USER_AGENTS_USER_TXT = get_abs_path("../user_agents.user.txt")
+DEFAULT_CONFIG_INI = get_abs_path("../config.ini")
+
+WECHAT_MSG = {0: "出现未知异常，程序中止", 1: "选课成功，课程为：", 2: "有名额，验证码识别失败，正在重试", 3: "出现重复选课，请调整config文件", "s": "刷课开始", 4: "时间冲突，课程为", 5: "考试时间冲突，课程为"}
+WECHAT_PREFIX = {0: "[异常]", 1: "[成功]", 2: "[失败]", 3: "[特殊]"}
+
+mkdir(CACHE_DIR)
+mkdir(CAPTCHA_CACHE_DIR)
+mkdir(LOG_DIR)
+mkdir(ERROR_LOG_DIR)
+mkdir(REQUEST_LOG_DIR)
+mkdir(WEB_LOG_DIR)
+
+if os.path.exists(USER_AGENTS_USER_TXT):
+    USER_AGENT_LIST = read_list(USER_AGENTS_USER_TXT)
+else:
+    USER_AGENT_LIST = read_list(USER_AGENTS_TXT_GZ)
+
+
+class IAAAURL(object):
+    """
+    Host
+    OauthHomePage
+    OauthLogin
+    """
+    Host = "iaaa.pku.edu.cn"
+    OauthHomePage = "https://iaaa.pku.edu.cn/iaaa/oauth.jsp"
+    OauthLogin = "https://iaaa.pku.edu.cn/iaaa/oauthlogin.do"
+
+
+class ElectiveURL(object):
+    """
+    Host
+    SSOLoginRedirect        重定向链接
+    SSOLogin                sso登录
+    Logout                  登出
+    HelpController          选课帮助页
+    ShowResults             选课结果页
+    SupplyCancel            补退选页
+    Supplement              补退选页第一页之后
+    DrawServlet             获取一张验证码
+    validate                补退选验证码校验接口
+    """
+    Scheme = "https"
+    Host = "elective.pku.edu.cn"
+    HomePage = "https://elective.pku.edu.cn/elective2008/"
+    SSOLoginRedirect = "http://elective.pku.edu.cn:80/elective2008/ssoLogin.do"
+    SSOLogin = "https://elective.pku.edu.cn/elective2008/ssoLogin.do"
+    Logout = "https://elective.pku.edu.cn/elective2008/logout.do"
+    HelpController = "https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/help/HelpController.jpf"
+    ShowResults = "https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/electiveWork/showResults.do"
+    SupplyCancel = "https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/supplement/SupplyCancel.do"
+    Supplement = "https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/supplement/supplement.jsp"
+    DrawServlet = "https://elective.pku.edu.cn/elective2008/DrawServlet"
+    Validate = "https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/supplement/validate.do"
---
+++ autoelective/logger.py
@@ -0,0 +1,98 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# filename: logger.py
+
+import os
+import logging
+from logging import StreamHandler
+from logging.handlers import TimedRotatingFileHandler
+from .config import AutoElectiveConfig
+from .const import ERROR_LOG_DIR
+from ._internal import mkdir
+from .notification.wechat_push import Notify
+from .const import WECHAT_MSG, WECHAT_PREFIX
+
+config = AutoElectiveConfig()
+notify = Notify(_disable_push=config.disable_push, _token=config.wechat_token, _interval_lock=config.minimum_interval, _verbosity=config.verbosity)
+
+_USER_ERROR_LOG_DIR = os.path.join(ERROR_LOG_DIR, config.get_user_subpath())
+mkdir(_USER_ERROR_LOG_DIR)
+
+
+class BaseLogger(object):
+    default_level = logging.DEBUG
+    default_format = logging.Formatter("[%(levelname)s] %(name)s, %(asctime)s, %(message)s", "%H:%M:%S")
+
+    def __init__(self, name, level=None, format=None):
+        if self.__class__ is __class__:
+            raise NotImplementedError
+        self._name = name
+        self._level = level if level is not None else self.__class__.default_level
+        self._format = format if format is not None else self.__class__.default_format
+        self._logger = logging.getLogger(self._name)
+        self._logger.setLevel(self._level)
+        self._logger.addHandler(self._get_handler())
+
+    @property
+    def handlers(self):
+        return self._logger.handlers
+
+    def _get_handler(self):
+        raise NotImplementedError
+
+    def log(self, level, msg, *args, **kwargs):
+        return self._logger.log(level, msg, *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        return self._logger.debug(msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        return self._logger.info(msg, *args, **kwargs)
+
+    def warn(self, msg, *args, **kwargs):
+        return self._logger.warn(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        return self._logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        if notify.get_verbosity == 2:
+            notify.send_wechat_push(token=notify.get_token, msg=str(msg), prefix=WECHAT_PREFIX[0])
+        return self._logger.error(msg, *args, **kwargs)
+
+    def exception(self, msg, *args, **kwargs):
+        kwargs.setdefault("exc_info", True)
+        return self._logger.exception(msg, *args, **kwargs)
+
+    def fatal(self, msg, *args, **kwargs):
+        notify.send_wechat_push(token=notify.get_token, msg=str(msg), prefix=WECHAT_PREFIX[0])
+        return self._logger.fatal(msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        return self._logger.critical(msg, *args, **kwargs)
+
+
+class ConsoleLogger(BaseLogger):
+    """ 控制台日志输出类 """
+
+    default_level = logging.DEBUG
+
+    def _get_handler(self):
+        handler = logging.StreamHandler()
+        handler.setLevel(self._level)
+        handler.setFormatter(self._format)
+        return handler
+
+
+class FileLogger(BaseLogger):
+    """ 文件日志输出类 """
+
+    default_level = logging.WARNING
+
+    def _get_handler(self):
+        file = os.path.join(_USER_ERROR_LOG_DIR, "%s.log" % self._name)
+        handler = TimedRotatingFileHandler(file, when='d', interval=1, encoding="utf-8-sig")
+        handler.setLevel(self._level)
+        handler.setFormatter(self._format)
+        return handler
